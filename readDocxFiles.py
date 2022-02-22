bbe6a9b9084ef3cc3d9d720f9320510f3bd96770
unknown <annau25@gmail.com>
2022-02-22 17:04:23 -0500

    commented workflow of readDocxFiles.py

--- readDocxFiles.py
+++ readDocxFiles.py
@@ -1,5 +1,18 @@
 import docx
 
+# The program should be able to take a .docx document as input
+# Then distinguish between tables and paragraph from the document
+# This program should be able to read the contents of the document,
+# and write them to another new document.
+
+# When the new document is finished writing, the Google Drive API will upload it to the Google Drive.
+
+# This is to test out how to perserve the structure of the input document
+
+# From working with this api, the paragraphs (docParagraphs) is an object.
+# There should be a way to convert it to a string so that it can be written
+# into the new document.
+
 # Gets all the paragraphs in a document
 def getParagraphs(document):
     docParagraphs = document.paragraphs
