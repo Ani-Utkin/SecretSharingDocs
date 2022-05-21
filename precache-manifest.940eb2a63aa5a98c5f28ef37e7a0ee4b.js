27d0dad811a311586ed884397f88620ac0fea8f2
Anna Utkin <annau25@gmail.com>
2022-05-21 16:49:42 -0400

    Updates

---
+++ precache-manifest.940eb2a63aa5a98c5f28ef37e7a0ee4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "15ffdccdc53f8654f5c1",
+    "url": "/SecretSharingDocs/static/css/main.5207ce26.chunk.css"
+  },
+  {
+    "revision": "15ffdccdc53f8654f5c1",
+    "url": "/SecretSharingDocs/static/js/main.15ffdccd.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/SecretSharingDocs/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "84f78fdffcdb61f75038",
+    "url": "/SecretSharingDocs/static/js/2.84f78fdf.chunk.js"
+  },
+  {
+    "revision": "e76487e2e3d6920289b2",
+    "url": "/SecretSharingDocs/static/js/runtime~main.e76487e2.js"
+  },
+  {
+    "revision": "86d18762597961bddc7a097840afb084",
+    "url": "/SecretSharingDocs/index.html"
+  }
+];
